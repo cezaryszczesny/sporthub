e5821040968a60df88519b3e0181ca2b95d5701c
Cezary Szczesny <c.szczesny@streamsoft.pl>
2023-01-09 19:39:57 +0100

    test db initialize scripts

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,3 @@
---CREATE BASE TABLES
-
 create table account
 (
     id          BIGSERIAL    NOT NULL,
@@ -136,45 +134,3 @@ alter table task
     add constraint task_id_operator_fkey foreign key (id_operator) references operator (id);
 alter table task
     add constraint task_id_facility_fkey foreign key (id_facility) references facility (id);
-
-
---wypełnienie danymi tablic stałych
-insert into coach_specialty
-values (1, 'Atak');
-insert into coach_specialty
-values (2, 'Pomoc');
-insert into coach_specialty
-values (3, 'Defensywa');
-insert into coach_specialty
-values (4, 'Bramkarz');
-insert into coach_specialty
-values (5, 'Wytrzymałość');
-
-insert into player_position
-values (1, 'Napastik');
-insert into player_position
-values (2, 'Pomocnik');
-insert into player_position
-values (3, 'Obrońca');
-insert into player_position
-values (4, 'Bramkarz');
-
-insert into player_foot
-values (1, 'Lewa');
-insert into player_foot
-values (2, 'Prawa');
-
-insert into player_status
-values (1, 'Aktywny');
-insert into player_status
-values (2, 'Kontuzjowany');
-insert into player_status
-values (3, 'Nieaktywny');
-
-insert into player_diet
-values (1, '3100');
-insert into player_diet
-values (2, '3400');
-insert into player_diet
-values (3, '3700');
-
